7de9dce1a50ea887f3f6e670c44d6ae7c807d95a
Liutka <liutauras_r@yahoo.com>
2019-12-10 19:47:09 +0200

    Added menu links

---
+++ src/main/java/com/computer/shop/controllers/ContactsController.java
@@ -0,0 +1,21 @@
+package com.computer.shop.controllers;
+
+import com.computer.shop.helpers.ViewHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+    @FXML
+    public void openMainMenu(final ActionEvent event) {
+        ViewHelper.loadView(this,"/menu/menu.fxml", "/menu/menu.css");
+        ViewHelper.killCurrentView(event);
+    }
+}
--- src/main/java/com/computer/shop/controllers/MenuController.java
+++ src/main/java/com/computer/shop/controllers/MenuController.java
@@ -2,9 +2,9 @@ package com.computer.shop.controllers;
 
 import com.computer.shop.helpers.ViewHelper;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,4 +20,25 @@ public class MenuController implements Initializable {
         ViewHelper.loadView(this, "/shopItems/shopItems.fxml", "/shopItems/shopItems.css");
         ViewHelper.killCurrentView(event);
     }
+    @FXML
+    public void loadContactsMenu(final ActionEvent event) {
+        ViewHelper.loadView(this, "/contactsMenu/contactsmenu.fxml", "/contactsMenu/contactsmenu.css");
+        ViewHelper.killCurrentView(event);
+    }
+    @FXML
+    public void backToMenu(final Event event) {
+        ViewHelper.loadView(this, "/login/login.fxml", "/login/login.css");
+        ViewHelper.killCurrentView(event);
+    }
+    @FXML
+    public void loadOrderMenu(final Event event) {
+        ViewHelper.loadView(this, "/orderMenu/ordermenu.fxml", "/orderMenu/ordermenu.css");
+        ViewHelper.killCurrentView(event);
+    }
+    @FXML
+    public void loadItemInsertion(final Event event) {
+        ViewHelper.loadView(this, "/iteminsertion/iteminsertion.fxml", "/iteminsertion/iteminsertion.css");
+        ViewHelper.killCurrentView(event);
+    }
+
 }
